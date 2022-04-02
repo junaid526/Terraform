7746bf91442f5d785380f83a55e3e225faaa56a0
junaid526 <80666143+junaid526@users.noreply.github.com>
2022-04-02 19:53:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
   region = "ap-south-1"
   access_key = "AKIA4YZ3PKUX7OS2VACI"
