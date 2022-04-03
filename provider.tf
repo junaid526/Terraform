78a891800582f61fc6c5ff417e7c4b63e4d0ce79
junaid526 <80666143+junaid526@users.noreply.github.com>
2022-04-03 19:56:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4YZ3PKUXYME4XB5F"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
