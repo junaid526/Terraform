fc5aac4db1caeb14652f330faaaa4404404ec469
junaid526 <80666143+junaid526@users.noreply.github.com>
2022-07-15 20:14:28 +0530

    update in main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-04c84f136b3c9d872"
-  instance_type = "t2.micro"
-  key_name   = "junnu-1"
+  instance_type = "t2.micr1"
+  key_name   = "junnu-11"
 
   tags = {
     Name = "new"
